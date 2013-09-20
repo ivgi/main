f7e483b27688785cf5787f3224785acc98747e10
ivgi <willpower1>
2013-09-20 10:08:41 +0300

    Exception package added

---
+++ Test/src/com/test/exceptions/ExceptionChecker.java
@@ -0,0 +1,12 @@
+package com.test.exceptions;
+
+public class ExceptionChecker {
+	
+	public static void main(String[] args) {
+		String number = "not_a_number";
+		//throws java.lang.NumberFormatException:
+		Integer realNum = Integer.parseInt(number);
+		
+	}
+
+}
